91daa5415d66dbfea1e27524e845edd7b0d6f510
Atanas Minev <atmin@pm.me>
2025-05-15 23:06:16 +0300

    touch inertia

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://atmin.me",
+    vite: {
+        server: {
+            allowedHosts: ['.ngrok-free.app'],
+        },
+    },
+    site: 'https://atmin.me',
 });
